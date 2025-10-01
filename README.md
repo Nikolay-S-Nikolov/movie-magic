0fa0ac880a0cbe14e0a917ff498bc8317ca371a3
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-10-01 10:03:25 +0300

    Add Handlebars

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 Express.js Workshop from JS Back-End SoftUni Course
 
 - [x] Initialize Project
-  - npm init -y
+  - `npm init -y`
   - update package.json
-    ```/
+    ```
     "start":"node src/index.js",
     "dev": "node --watch src/index.js"
     "type": "module"
     ```
-- [x] Add Express Server npm i express
+- [x] Add Express Server `npm i express`
+- [x] Add Workshop Resources
+- [x] Add Handlebars `npm i express-handlebars`
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 const PORT = 5000;
 
+
+// Handlebars setup
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('home', { layout: false });
 });
 
+// Server start
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
