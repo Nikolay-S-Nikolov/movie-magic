f62296fc4a2ef7f6e741942418fc94d2582efd65
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-10-02 16:20:24 +0300

    Add get movie by id to movie service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,6 +5,10 @@ export default {
         return Movie.find();
     },
 
+    getOne(movieId) {
+        return Movie.find({_id: movieId});
+    },
+
     create(movieData){
         movieData.rating = Number(movieData.rating);
         const movie = new Movie(movieData);
